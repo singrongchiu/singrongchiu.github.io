0dfa5cc7c1c53d742d1e1992b31615df2ad58f9a
eshau <ehau00@gmail.com>
2026-02-06 19:20:28 -0500

    ok

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ uv venv --python 3.12
 source .venv/bin/activate
 ```
 
+# TO COMPRESS (AND COMPARE SIZES)
+```
+sh compress.sh
+```
+
 # TO RUN
 ```
-sh run.sh src
+sh run.sh src.tar.br
 ```
---
+++ compress.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+tar -cf - src | brotli -q 11 -w 24 > src.tar.br
+
+tar -cf - src | gzip -9 > src.tar.gz
+
+tar -cf - src | zstd --ultra -22 > src.tar.zst
+
+for f in src.tar.br src.tar.gz src.tar.zst; do
+  stat -f %z "$f" | awk -v name="$f" '{ printf "%s : %.2f KB\n", name, $1/1024 }'
+done
